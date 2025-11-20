d3609fa431186460622827b969b54c6db448acd4
Cruelasss <irula5198@gmail.com>
2025-11-20 17:58:39 +0700

    tambah list items berisi Pair untuk menampilkan detail siswa

--- app/src/main/java/com/example/mvvm/view/uicontroller/TampilSiswa.kt
+++ app/src/main/java/com/example/mvvm/view/uicontroller/TampilSiswa.kt
@@ -2,6 +2,7 @@ package com.example.mvvm.view.uicontroller
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 
 @OptIn(markerClass = ExperimentalMaterial3Api::class)
 @Composable
@@ -10,3 +11,10 @@ fun TampilSiswa(
     statusUISiswa: Siswa,
     onBackButtonClicked: () -> Unit
 )
+{
+    //edit 2 : items Pair
+    val items = listOf(
+        Pair(first = stringResource("Nama Lengkap"), second = statusUISiswa.nama),
+        Pair(first = stringResource(id = "Jenis Kelamin"), second = statusUISiswa.gender),
+        Pair(first = stringResource("Alamat"), second = statusUISiswa.alamat)
+    )
