5b99d96cb654f14adfd9a3e663ce3fef9de56e0f
Cruelasss <irula5198@gmail.com>
2025-11-20 18:04:40 +0700

    tampilkan daftar detail siswa dengan Text berformat dan Divider tiap item

--- app/src/main/java/com/example/mvvm/view/uicontroller/TampilSiswa.kt
+++ app/src/main/java/com/example/mvvm/view/uicontroller/TampilSiswa.kt
@@ -3,6 +3,7 @@ package com.example.mvvm.view.uicontroller
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -14,7 +15,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mvvm.R
 
 @OptIn(markerClass = ExperimentalMaterial3Api::class)
@@ -52,3 +55,10 @@ fun TampilSiswa(
                 verticalArrangement = Arrangement.spacedBy(space = dimensionResource(16.dp))
             ) {
         }
+            items.forEach { item ->
+                Column {
+                    Text(text = item.first.uppercase(), fontSize = 16.sp)
+                    Text(text = item.second, fontWeight = FontWeight.Bold, fontSize = 16.sp)
+                }
+                Divider(thickness = dimensionResource(1.dp))
+            }
