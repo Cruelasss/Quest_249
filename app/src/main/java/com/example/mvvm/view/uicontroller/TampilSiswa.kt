d924feead87c9b51bad7c3df86fcc691b3a7c2d4
Cruelasss <irula5198@gmail.com>
2025-11-20 18:01:02 +0700

    implement Scaffold dengan TopAppBar bertema ungu untuk halaman Data Siswa

--- app/src/main/java/com/example/mvvm/view/uicontroller/TampilSiswa.kt
+++ app/src/main/java/com/example/mvvm/view/uicontroller/TampilSiswa.kt
@@ -1,7 +1,9 @@
 package com.example.mvvm.view.uicontroller
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 
 @OptIn(markerClass = ExperimentalMaterial3Api::class)
@@ -17,4 +19,14 @@ fun TampilSiswa(
         Pair(first = stringResource("Nama Lengkap"), second = statusUISiswa.nama),
         Pair(first = stringResource(id = "Jenis Kelamin"), second = statusUISiswa.gender),
         Pair(first = stringResource("Alamat"), second = statusUISiswa.alamat)
+
+
+    )
+    Scaffold(modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = "Data Siswa"), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = colorResource(id = R.color.purple_500)),
+            )
+        }
     )
